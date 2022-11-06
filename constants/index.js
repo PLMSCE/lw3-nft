0d9f0b970e602d28e2c6f7225cca2fc2fe459a7c
PLMSCE <111319638+PLMSCE@users.noreply.github.com>
2022-11-05 21:17:31 -0400

    Rev0 (#2)
    * Learnweb3 NFT-Collection Project.
    
    * final changes

--- constants/index.js
+++ constants/index.js
@@ -1,5 +1,7 @@
 const WHITELIST_CONTRACT_ADDRESS = "0x3948c33dF779C126C6387ae30315dAe1930e1B4D";
 
-const METADATA_URL = "";
+
+const METADATA_URL = "https://lw3-nft.vercel.app/api/";
+
 
 module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
