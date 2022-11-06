655d06a6b983adc53a38b19f51ea58ef7ab34c30
PLMSCE <111319638+PLMSCE@users.noreply.github.com>
2022-11-05 20:56:04 -0400

    Learnweb3 NFT-Collection Project. (#1)

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,16 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config({ path: "/Users/admin/Desktop/nft-collection-2/lw3-nft/.env" });
 
-/** @type import('hardhat/config').HardhatUserConfig */
+const GOERLI_URL = process.env.GOERLI_URL_API_KEY;
+
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
+
 module.exports = {
-  solidity: "0.8.17",
+  solidity: "0.8.4",
+  networks: {
+    goerli: {
+      url: GOERLI_URL,
+      accounts: [PRIVATE_KEY]
+    },
+  },
 };
